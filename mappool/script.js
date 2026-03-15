7ce805500c78ebf4e80db70f0b85d1e4ca81dcf8
tim25871014 <tim25871014@gmail.com>
2026-03-15 20:00:43 +0800

    change socket route

--- mappool/script.js
+++ mappool/script.js
@@ -1,6 +1,6 @@
 let stageInfo, mappool, players;
-//let socket = new ReconnectingWebSocket('ws://127.0.0.1:24050/ws');
-let socket = new ReconnectingWebSocket('ws://127.0.0.1:3000/ws');
+let socket = new ReconnectingWebSocket('ws://127.0.0.1:24050/ws');
+//let socket = new ReconnectingWebSocket('ws://127.0.0.1:3000/ws');
 
 (async () => {
     $.ajaxSetup({ cache: false });
